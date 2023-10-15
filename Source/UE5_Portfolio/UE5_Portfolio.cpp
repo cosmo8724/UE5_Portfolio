f3390d642180c69295c11eb060f5b5d027a236c2
Cosmo <dlwogh1204@gmail.com>
2023-10-16 04:23:01 +0900

    Initialize_Project

---
+++ Source/UE5_Portfolio/UE5_Portfolio.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Portfolio.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Portfolio, "UE5_Portfolio" );
+ 
